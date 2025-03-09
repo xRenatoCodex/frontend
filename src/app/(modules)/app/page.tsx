27a0e4c4e5937d1e7315ff9dce65b430a5180c97
xRenatoCodex <renatozapata199788@gmail.com>
2025-03-09 17:49:40 -0500

    creacion de componente serach input

--- src/app/(modules)/app/page.tsx
+++ src/app/(modules)/app/page.tsx
@@ -2,6 +2,7 @@
 
 
 import { SelectFolder } from "@/components/app";
+import SearchInput from "@/components/utils/search-input";
 
 export default function Page() {
 
@@ -9,6 +10,13 @@ export default function Page() {
         <div className="relative flex flex-col justify-start items-center w-full h-full gap-4">
             <div className="absolute top-0 left-2 h-12 w-auto flex flex-row justify-start items-center gap-4">
                 <SelectFolder />
+            </div>
+            <div className="flex flex-col justify-start items-center w-full h-full gap-4">
+
+            </div>
+            <div className="absolute bottom-0 left-2 h-12 w-auto flex flex-row justify-start items-center gap-4">
+                <SearchInput />
+
             </div>
         </div>
     );
--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -1,2 +1,4 @@
+
 export {default as SideBar} from "./sidebar";
 export { default as Avatar } from "./avatar";
+export { default as Button } from "./search-input";
