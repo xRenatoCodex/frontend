42698b2baa84fc1e54502e2ff6cf2f04c5fa23c4
renatozapata199788 <renatozapata199788@gmail.com>
2025-03-09 15:55:04 +0000

    config

--- src/components/app/select-folder.tsx
+++ src/components/app/select-folder.tsx
@@ -6,8 +6,8 @@ export default function Component() {
 
 
     return (
-        <div className="relative flex flex-row justify-start items-center h-full w-auto rounded-xl p-2 bg-[#21272f]">
-            <div className="flex flex-row justify-start items-center h-full flex-2/3 gap-2 border-solid border-r-1 border-gray-700">
+        <div  className="relative flex flex-row justify-start items-center h-full w-auto rounded-xl p-2 bg-[#21272f]">
+            <div  className="flex flex-row justify-start items-center h-full flex-2/3 gap-2 border-solid border-r-1 border-gray-700">
                 <FolderRoundedIcon sx={{ color: '#00A76F' }} />
                 <Typography  >Folderasdasd</Typography>
             </div>
